a31b2f0f9f1b44ff059e0a21e1125a85ac21f2d7
cfabregas85 <ing.cfabregas@gmail.com>
2019-10-09 07:16:07 -0700

    First Commit

---
+++ Data/PaymentDetailContext.cs
@@ -0,0 +1,21 @@
+﻿using APIPayment.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPayment.Data
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {             
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+
+
+    }
+}
